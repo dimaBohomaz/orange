fa245c0d8053c8f31392f9a9c590196b7e84fb99
Dima Bohomaz <dmytrobohomaz@gmail.com>
2018-11-11 20:32:19 +0200

    second commit in the junior branch

--- orange.js
+++ orange.js
@@ -1,5 +1,6 @@
 'use strict';
 let orange = document.querySelectorAll('.orange')[0];
 orange.onclick = function() {
-	this.style.cssText = 'width: 170px;height: 170px; background: #FAB410'
+	//this.style.cssText = 'width: 150px;height: 150px;';
+	this.className = 'grad'
 }
